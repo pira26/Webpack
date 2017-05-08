50cfe2244a3b26065b68cb0efa57e1bbe8b84548
Pira <sritharan.pirathaab@gmail.com>
2017-05-08 17:06:21 +0200

    webpack 2 config

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import webpackImg from '../../dist/assets/webpack.png';
+
 const App = () => {
   return (
     <div>
-      <h1>Webpack Config</h1>
-      <h3>TEST</h3>
-      <p>success</p>
+      <div className="webpack">
+        <h1>Webpack Config</h1>
+        <h3>TEST</h3>
+        <p>success</p>
+        <div className="webpack-image">
+          <img src={webpackImg} />
+        </div>
+      </div>
     </div>
   );
 }
